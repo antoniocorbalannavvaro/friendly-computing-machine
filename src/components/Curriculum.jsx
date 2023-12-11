72907f7966ebe4d69e67e5d49a77da100d7e913f
Antonio Corbalán Navarro <antoniocn1996@gmail.com>
2023-12-11 07:53:52 +0100

    cv updated

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -4,7 +4,7 @@ const Curriculum = () => {
   return (
     <div className={styles.cv}>
 
-      <object data='https://drive.google.com/file/d/1wCKwYCrQK6uNzHt89G9br-tUNOKr0qFr/preview?usp=sharing' width='100%' height='100%' />
+      <object data='https://drive.google.com/file/d/1w8ccQBlCm3dk8swgFa7ATj5HMTnwrZIJ/preview?usp=sharing' width='100%' height='100%' />
     </div>
   )
 }
